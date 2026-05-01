1024949d935a312393441abe1cf64346771c41cc
AT-dev-dds <ashutoshmishra.dds@gmail.com>
2026-05-01 15:57:58 +0530

    Nested Layout Practice

---
+++ routing-app/app/products/[productId]/layout.tsx
@@ -0,0 +1,13 @@
+
+
+export default function productLayout({children,}:
+    {
+        children: React.ReactNode    
+    }){
+   return (
+    <>
+    {children}
+    <h2>Featured Nested Layout</h2>
+    </>
+   )
+}
