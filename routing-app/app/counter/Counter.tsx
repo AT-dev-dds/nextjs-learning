8225b90fb6b416944fef788a9533dd3876af3882
AT-dev-dds <ashutoshmishra.dds@gmail.com>
2026-05-02 11:59:41 +0530

    Applied solution for metadata limitation

---
+++ routing-app/app/counter/Counter.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React, { useState } from 'react'
+
+export const metadata={
+    title:"Counter"
+}
+
+export default function counter() {
+    
+    const [counter,setCounter]=useState(0);
+    return(
+        <div>
+      <button onClick={()=>{
+        setCounter(counter+1)
+      }}>Increase count</button>
+    </div>
+  )
+}
--- routing-app/app/counter/page.tsx
+++ routing-app/app/counter/page.tsx
@@ -1,19 +1,10 @@
-"use client";
+import Counter from './Counter'
 
-import React, { useState } from 'react'
 
-// export const metadata={
-//     title:"Counter"
-// }
+export const metadata={
+    title:"Counter"
+}
 
-export default function counter() {
-    
-    const [counter,setCounter]=useState(0);
-    return(
-        <div>
-      <button onClick={()=>{
-        setCounter(counter+1)
-      }}>Increase count</button>
-    </div>
-  )
+export default function CounterPage() {
+    return <Counter/>
 }
