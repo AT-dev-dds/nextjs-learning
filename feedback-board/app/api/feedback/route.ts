755d81e168635bb441c2042ede2ecc075e3b776a
AT-dev-dds <ashutoshmishra.dds@gmail.com>
2026-05-06 11:13:25 +0530

    feat: Created new project for implementing and understanding Route handlers, revalidate and folder structure .
    Also implemented route.ts for API creation

---
+++ feedback-board/app/api/feedback/route.ts
@@ -0,0 +1,25 @@
+let feedbacks=[
+    {
+        id:1,
+        message:"Nice app!"
+    }
+];
+
+export function GET(){
+    return Response.json(feedbacks);
+};
+
+
+export async function POST (request:Request){
+    const body= await request.json();
+    const date=new Date();
+    const newFeedback={
+        id:date.getDate(),
+        message:body.message,
+    }
+
+    feedbacks.push(newFeedback);
+
+    return Response.json(feedbacks);
+} ;
+
