4a5701e07f46f48badbd3dcb0d500484f49cac12
AT-dev-dds <ashutoshmishra.dds@gmail.com>
2026-05-08 12:26:33 +0530

    feat: POST request and added data collection in mongoDB

--- feedback-board/app/api/feedback/route.ts
+++ feedback-board/app/api/feedback/route.ts
@@ -1,12 +1,34 @@
-
 import client from '@/lib/mongodb'
+
+// export let feedbacks=[
+//     {
+//         id:1,
+//         message:"Nice app!"
+//     }
+// ];
 
 export async function GET(){
     await client.connect();
 
     const db= client.db("feedbackDB");
 
-    const feedbacks= db.collection("feedbacks").find().toArray();
+    const feedbacks= await db.collection("feedbacks").find().toArray();
 
-    return Response.json(feedbacks)
+    return Response.json(feedbacks);
+};
+
+
+export async function POST(request:Request){
+
+    const body= await request.json();
+    await client.connect();
+
+    const db=client.db("feedbackDB");
+
+    const result= await db.collection("feedbacks").insertOne({
+        message:body.message,
+    });
+
+
+    return Response.json(result);
 }
