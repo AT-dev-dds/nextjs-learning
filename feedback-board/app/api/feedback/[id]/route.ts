76f04c277c589ac1fa0c2863aff91bae0e7b1dd9
AT-dev-dds <ashutoshmishra.dds@gmail.com>
2026-05-08 14:41:44 +0530

    Removed Comment

--- feedback-board/app/api/feedback/[id]/route.ts
+++ feedback-board/app/api/feedback/[id]/route.ts
@@ -1,24 +1,4 @@
 
-// import {feedbacks} from '../route'
-
-// export async function DELETE(_request:Request,{params}:{params:Promise<{id:string}>}){
-
-//    const {id}= await params;
-
-//    const Id=parseInt(id);
-
-//   const index=feedbacks.findIndex((f)=>f.id===Id);
-
-//   if(index===-1){
-//     return Response.json({message:"Not found"},{status:404});
-//   }
-
-//   const deleted= feedbacks.splice(index,1);
-
-//   return Response.json({message:"Deleted Successfully!",deletedFeedback:deleted[0]});
-// };
-
-
 import client from '@/lib/mongodb'
 import { ObjectId } from 'mongodb';
 
@@ -72,21 +52,3 @@ export async function PUT(request:Request,{params}:{params:Promise<{id:string}>}
 
 
 
-
-// export async function PUT(request:Request,{params}:{params:Promise<{id:string}>}){
-
-//   const {id}=await params;
-//   const Id=parseInt(id);
-
-//   const body=await request.json();
-
-//   const feedback= feedbacks.find((f)=>f.id===Id);
-
-//   if(!feedback){
-//     return Response.json({message:"Feedback not found!"},{status:404});
-//   }
-
-//   feedback.message=body.message;
-
-//   return Response.json(feedback);
-// }
